1363ce6723ab10892207c64111ef84c91aa7a4e7
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-28 02:56:25 +0100

    style:added the hover effect to the mobile links

--- Components/MenuBar.jsx
+++ Components/MenuBar.jsx
@@ -5,21 +5,21 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 const MenuBar = () => {
   return (
     <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
-      <div className="max-w-[1240] m-auto flex justify-between items-center p-4 text-white">
+      <div className="max-w-[1240] m-auto flex justify-between items-center p-4 text-black">
         <Link href="/">
           <h1 className="font-bold text-4xl">Capture</h1>
         </Link>
         <ul className=" hidden sm:flex">
-          <li className="p-4">
+          <li className="p-4 text-3xl">
             <Link href="/">Home</Link>
           </li>
-          <li className="p-4">
+          <li className="p-4 text-3xl">
             <Link href="/#gallery">Gallery</Link>
           </li>
-          <li className="p-4">
+          <li className="p-4 text-3xl">
             <Link href="/portfolio">Work</Link>
           </li>
-          <li className="p-4">
+          <li className="p-4 text-3xl">
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
@@ -28,18 +28,18 @@ const MenuBar = () => {
           <AiOutlineMenu size={20} />
         </div>
         {/* mobile menu */}
-        <div className="sm:hidden left-0 top-0 right-0 bottom-0 flex justify-center items-center w-full h-screen">
+        <div className="sm:hidden left-0 top-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300">
           <ul>
-            <li>
+            <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/">Home</Link>
             </li>
-            <li>
+            <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/#gallery">Gallery</Link>
             </li>
-            <li>
+            <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/portfolio">Work</Link>
             </li>
-            <li>
+            <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
