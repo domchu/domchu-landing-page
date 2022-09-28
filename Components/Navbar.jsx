fd4fab9516a44c4e8ac1259cc9d5ad255161350e
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-28 01:15:43 +0100

    style:added the opacity to the banner

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex text-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
       {/* overlay */}
-      <div />
+      <div className="absolute top-0 left-0 bottom-0 bg-black/70 z-[4]" />
       <div>
         <h2>heading</h2>
         <p>message</p>
