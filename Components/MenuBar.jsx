6108fc57b10624d054851cc7bcf19b65cc56dea6
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-28 02:40:45 +0100

    style:styling the mobile view

--- Components/MenuBar.jsx
+++ Components/MenuBar.jsx
@@ -9,26 +9,26 @@ const MenuBar = () => {
         <Link href="/">
           <h1 className="font-bold text-4xl">Capture</h1>
         </Link>
-        <ul>
-          <li>
+        <ul className=" hidden sm:flex">
+          <li className="p-4">
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/#gallery">Gallery</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/portfolio">Work</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
         {/* mobile button */}
-        <div>
+        <div className="block sm:hidden z-10 cursor-pointer">
           <AiOutlineMenu size={20} />
         </div>
         {/* mobile menu */}
-        <div>
+        <div className="sm:hidden left-0 top-0 right-0 bottom-0 flex justify-center items-center w-full h-screen">
           <ul>
             <li>
               <Link href="/">Home</Link>
