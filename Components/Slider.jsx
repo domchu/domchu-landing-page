de3271082569399789149588c4bebfc1d2709370
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-29 02:49:18 +0100

    chore: function for the slider both next and prev

--- Components/Slider.jsx
+++ Components/Slider.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { sliderData } from "./SliderData";
 
 const Slider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const PrevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  if (!Array.isArray === 0 || current <= 0) {
+    return null;
+  }
   return (
     <div id="gallery">
       <h1>Gallery</h1>
       <div className="slider">
         {sliderData.map((slide, index) => {
           return (
-            <Image src={slide.image} alt="/" width="1440px" height="600px" />
+            <div key={index} className="">
+              <Image
+                src={slide.image}
+                alt="/"
+                width="1440"
+                height="600"
+                objectFit="cover"
+              />
+            </div>
           );
         })}
       </div>
