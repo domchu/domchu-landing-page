27859351124383cae94d4772fbfa93e93c2698e3
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-30 01:53:44 +0100

    bug:fixed the image path

--- Components/Portfolio.js
+++ Components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import nature1 from "../public/nature1.jpg";
 
 const Portfolio = () => {
   return (
@@ -7,7 +8,53 @@ const Portfolio = () => {
       <h1>Travel Photos</h1>
       <div>
         <div>
-          <Image />
+          <Image
+            src={nature1}
+            alt="/"
+            layout="responsive"
+            width="650"
+            height="450"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="/public/nature1.jpg"
+            alt="/"
+            layout="responsive"
+            width="220"
+            height="230"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="/public/nature1.jpg"
+            alt="/"
+            layout="responsive"
+            width="220"
+            height="230"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="/public/nature1.jpg"
+            alt="/"
+            layout="responsive"
+            width="220"
+            height="230"
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-full">
+          <Image
+            src="/public/nature1.jpg"
+            alt="/"
+            layout="responsive"
+            width="220"
+            height="230"
+            objectFit="cover"
+          />
         </div>
       </div>
     </div>
