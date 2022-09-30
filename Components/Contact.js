d83e321bf3ff1bdb96791ba7b024131fe248b73f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-01 00:06:05 +0100

    style:style the contact form

--- Components/Contact.js
+++ Components/Contact.js
@@ -2,22 +2,39 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Let's work together</h1>
-      <form action="">
-        <div>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
+    <div className="max-w-[1540px] m-auto p-4 h-screen bg-gray-700">
+      <h1 className="text-2xl font-bold text-center p-4 text-white">
+        Let's work together
+      </h1>
+      <form className="max-w-[600px] m-auto">
+        <div className="grid grid-cols-2 gap-2">
+          <input
+            className="border-shallow-lg p-3"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            className="border-shallow-lg p-3"
+            type="email"
+            placeholder="Email"
+          />
         </div>
-        <input type="text" placeholder="Subject" />
+        <input
+          className="border-shallow-lg p-3 w-full my-2"
+          type="text"
+          placeholder="Subject"
+        />
         <textarea
+          className="border-shallow-lg p-3 w-full"
           name=""
           id=""
           cols="30"
           rows="10"
           placeholder="Message"
         ></textarea>
-        <button>Summit</button>
+        <button className="border-shallow-lg p-3 w-full mt-4 text-white">
+          Summit
+        </button>
       </form>
     </div>
   );
