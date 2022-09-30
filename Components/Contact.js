cf6f0d92b13bdafa7b579eff7d6e9b0259690936
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-30 23:44:21 +0100

    form:added the form attribute

--- Components/Contact.js
+++ Components/Contact.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1>Let's work together</h1>
+      <form action="">
+        <div>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+        </div>
+        <input type="text" placeholder="Subject" />
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button>Summit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
