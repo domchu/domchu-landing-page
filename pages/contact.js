9e0fcf835d4aee213332f2eb7f48b70bd2c389a7
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-30 15:04:29 +0100

    import the navbar to the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Contact from "../Components/Contact";
+import Navbar from "../Components/Navbar";
 
 const contact = () => {
-  return <div>contact</div>;
+  return (
+    <div>
+      <Navbar />
+      <Contact />
+    </div>
+  );
 };
 
 export default contact;
