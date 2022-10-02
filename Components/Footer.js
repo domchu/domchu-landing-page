9286b7e6f1aaa656d281afca8b971f68c9585fbe
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-02 14:31:17 +0100

    link:  put links to the footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,8 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer>
+      <div className="bg-gray-800 text-white flex justify-center">
+        <ul style={{}} className=" block text-1xl sm:flex">
+          <li className="p-4 text-2xl">
+            <Link href="/">Home</Link>
+          </li>
+
+          <li className="p-4 text-2xl">
+            <Link href="/work">Work</Link>
+          </li>
+          <li className="p-4 text-2xl">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div>
+          <li className="p-4 text-2xl flex justify-end text-end">
+            <Link href="/#gallery">Gallery</Link>
+          </li>
+        </div>
+      </div>
       <p className="text-center p-4 text-1xl font-bold/30 bg-gray-800 text-white">
         &copy; copy, All right reserved designed Mr Dominic 2022 |
         next/react-Landing Page
