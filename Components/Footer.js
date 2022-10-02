5d87d8cfd4a1475d4d1fa94242e4c60e269e0f3b
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-02 14:10:53 +0100

    added the texts to the footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p className="text-center p-4 text-1xl font-bold/30 bg-gray-800 text-white">
+        &copy; copy, All right reserved designed Mr Dominic 2022 |
+        next/react-Landing Page
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
